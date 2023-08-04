5b6359c36879591b572d5e720d9df09357514198
Marcus Lindemann <mli@NomadBSD.shebang.de>
2023-08-04 20:01:35 +0200

    Copy template configuration after package installation.

--- post_install.sh
+++ post_install.sh
@@ -3,5 +3,11 @@
 # setup the mail server after installation of packages
 #
 
+# copy the initial dovecot configuration from the examples.
+# NOTE: The user is responsible for properly configuring dovecot
+cd /usr/local/etc/dovecot
+cp -r example-config/* .
+
+# enable dovecot at system (jail) start and start it first time
 sysrc dovecot_enable=YES
 service dovecot start
